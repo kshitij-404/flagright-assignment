378b830757b2a78eedd37e1d214a2189783f3835
Kshitij Ayush <kshitij4ka@gmail.com>
2024-09-20 21:52:39 +0530

    add/cors
    Add cors package and configure CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { connectDB } from "./config/database";
 import bodyParser from "body-parser";
+import cors from "cors";
 import helloRoute from "./routes/sample";
 import transactionRoute from "./routes/transaction";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
